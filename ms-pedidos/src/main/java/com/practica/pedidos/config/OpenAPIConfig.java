b4359349e96bf6adbac0eb737603bed0574735df
DincaAlex <alexandruandrei.dinca@unmsm.edu.pe>
2025-11-03 01:33:20 -0500

    swagger agregado

---
+++ ms-pedidos/src/main/java/com/practica/pedidos/config/OpenAPIConfig.java
@@ -0,0 +1,45 @@
+package com.practica.pedidos.config;
+
+import io.swagger.v3.oas.models.OpenAPI;
+import io.swagger.v3.oas.models.info.Contact;
+import io.swagger.v3.oas.models.info.Info;
+import io.swagger.v3.oas.models.info.License;
+import io.swagger.v3.oas.models.servers.Server;
+import org.springframework.beans.factory.annotation.Value;
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+
+import java.util.List;
+
+@Configuration
+public class OpenAPIConfig {
+
+    @Value("${server.port:8082}")
+    private String serverPort;
+
+    @Bean
+    public OpenAPI pedidosOpenAPI() {
+        Server devServer = new Server();
+        devServer.setUrl("http://localhost:" + serverPort);
+        devServer.setDescription("Development Server");
+
+        Contact contact = new Contact();
+        contact.setName("API Support");
+        contact.setEmail("support@example.com");
+
+        License license = new License()
+                .name("MIT License")
+                .url("https://choosealicense.com/licenses/mit/");
+
+        Info info = new Info()
+                .title("Pedidos API")
+                .version("1.0.0")
+                .description("API REST reactiva para la gestión de pedidos")
+                .contact(contact)
+                .license(license);
+
+        return new OpenAPI()
+                .info(info)
+                .servers(List.of(devServer));
+    }
+}
---
+++ ms-productos/src/main/java/com/practica/productos/config/OpenAPIConfig.java
@@ -0,0 +1,45 @@
+package com.practica.productos.config;
+
+import io.swagger.v3.oas.models.OpenAPI;
+import io.swagger.v3.oas.models.info.Contact;
+import io.swagger.v3.oas.models.info.Info;
+import io.swagger.v3.oas.models.info.License;
+import io.swagger.v3.oas.models.servers.Server;
+import org.springframework.beans.factory.annotation.Value;
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+
+import java.util.List;
+
+@Configuration
+public class OpenAPIConfig {
+
+    @Value("${server.port:8081}")
+    private String serverPort;
+
+    @Bean
+    public OpenAPI productosOpenAPI() {
+        Server devServer = new Server();
+        devServer.setUrl("http://localhost:" + serverPort);
+        devServer.setDescription("Development Server");
+
+        Contact contact = new Contact();
+        contact.setName("API Support");
+        contact.setEmail("support@example.com");
+
+        License license = new License()
+                .name("MIT License")
+                .url("https://choosealicense.com/licenses/mit/");
+
+        Info info = new Info()
+                .title("Productos API")
+                .version("1.0.0")
+                .description("API REST reactiva para la gestión de productos")
+                .contact(contact)
+                .license(license);
+
+        return new OpenAPI()
+                .info(info)
+                .servers(List.of(devServer));
+    }
+}
