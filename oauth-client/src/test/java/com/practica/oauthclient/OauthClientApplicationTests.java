cc6384f4ceabfdf637e0bfc302fe40a78a9048a2
DincaAlex <alexandruandrei.dinca@unmsm.edu.pe>
2025-11-23 19:43:05 -0500

    test sonarcloud 4: the final season part 1

--- oauth-client/src/test/java/com/practica/oauthclient/OauthClientApplicationTests.java
+++ oauth-client/src/test/java/com/practica/oauthclient/OauthClientApplicationTests.java
@@ -2,8 +2,10 @@ package com.practica.oauthclient;
 
 import org.junit.jupiter.api.Test;
 import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.test.context.ActiveProfiles;
 
 @SpringBootTest
+@ActiveProfiles("test")
 class OauthClientApplicationTests {
 
 	@Test
