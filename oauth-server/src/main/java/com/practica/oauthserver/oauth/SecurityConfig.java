7cf22c453a1997b9a892881d62e21d11b60ed76f
DincaAlex <alexandruandrei.dinca@unmsm.edu.pe>
2025-11-23 20:47:09 -0500

    fix: add missing Profile import in oauth-server SecurityConfig

--- oauth-server/src/main/java/com/practica/oauthserver/oauth/SecurityConfig.java
+++ oauth-server/src/main/java/com/practica/oauthserver/oauth/SecurityConfig.java
@@ -14,6 +14,7 @@ import com.nimbusds.jose.proc.SecurityContext;
 
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
+import org.springframework.context.annotation.Profile;
 import org.springframework.core.annotation.Order;
 import org.springframework.http.MediaType;
 import org.springframework.security.config.Customizer;
